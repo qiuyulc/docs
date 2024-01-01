f239d2ab7f77196976bdbd3586754021cbdf8fcd
Mrlishizhen <718647063@qq.com>
2024-01-01 22:31:03 +0800

    first commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,49 @@
+import { defineUserConfig, defaultTheme } from "vuepress";
+
+export default defineUserConfig({
+  base: "/docs/",
+  lang: "zh-CN",
+  title: "前端学习记录",
+  description: "前端学习复习路线图",
+  theme: defaultTheme({
+    // 默认主题配置
+    home: "/",
+    notFound: ["峰回路转多歧路,你走丢了哦！"],
+    backToHome: "返回首页",
+    navbar: [
+      {
+        text: "前端基础",
+        children: [
+          {
+            text: "html",
+            link: "/html/introduction",
+          },
+        ],
+      },
+    ],
+    sidebar: {
+      "/html/": [
+        {
+          text: "基础",
+          collapsible: true,
+          children: ["/html/introduction", "/html/base-elements"],
+        },
+      ],
+      // "/html/introduction", "/html/base-elements"
+      //   {
+      //         text: "HTML",
+      //         link: "/html",
+      //         children: [
+      //             {
+      //                 text: "认识HTML",
+      //                 link: "/html/introduction",
+      //             },
+      //             {
+      //                 text: "基础元素",
+      //                 link: "/html/base-elements",
+      //             },
+      //         ],
+      //     },
+    },
+  }),
+});
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+home: true
+heroText: 复习中...
+heroImage: https://vuejs.org/images/logo.png
+actions:
+  - text: 快速开始
+    link: /HTML
+    type: primary
+  - text: 项目简介
+    link: /Introduction
+    type: secondary
+
+features:
+  - title: HTML
+    details: Hypertext Markup Language,创建和组织网页内容的标记语言。
+footer: 前端复习文档，欢迎交流。
+---
+
+<!--
+    home boolean 首页的大标题。
+    homeText string | null 首页的大标题。 默认使用站点 title
+    tagline string | null 首页的标语。 默认使用站点 description 。
+
+ -->
